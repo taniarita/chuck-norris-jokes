4948873ccb476f2f34562fa9c0c6bb3c3a5446d4
Tânia Rita Mendes de Camargo <CIANDT\taniarita@lnb035048cps.ciandt.global>
2022-04-20 11:04:00 -0300

    adding class JokeViewModel

--- chucknorrisjoke/app/src/main/java/com/example/chucknorris_joke/Endpoint.kt
+++ chucknorrisjoke/app/src/main/java/com/example/chucknorris_joke/Endpoint.kt
@@ -6,5 +6,5 @@ import retrofit2.http.GET
 interface Endpoint {
 
     @GET("jokes/random") //adiciona os endpoints necessários para consumir os dados da API
-    fun getJokes() : Call<JokeModel> // cria o endpoint que utiliza o verbo GET e retorna um objeto retrofit do tipo Call que armazena uma lista do objetos do tipo JokeModel(modelo)
+    fun getJokes(): Call<JokeModel> // cria o endpoint que utiliza o verbo GET e retorna um objeto retrofit do tipo Call que armazena uma lista do objetos do tipo JokeModel(modelo)
 }
